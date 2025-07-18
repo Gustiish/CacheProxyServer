c56ffb24fd20d30fc67afa9e9ad2a208d8acb462
Gustiish <isak.backstrom@gmail.com>
2025-07-18 15:10:21 +0200

    Fixed cache

--- Commands.cs
+++ Commands.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -27,8 +28,9 @@ namespace CacheProxyServer
                 var factory = prop.GetRequiredService<IHttpClientFactory>();
                 var client = factory.CreateClient();
                 client.BaseAddress = new Uri(origin);
+                IMemoryCache cache = new MemoryCache(new MemoryCacheOptions());
 
-                return new Forwarder(client);
+                return new Forwarder(client, cache);
             });
 
 
@@ -37,25 +39,23 @@ namespace CacheProxyServer
             Console.WriteLine($"URL is set to {origin}");
 
             app.MapGet("{**catchAll}", async (HttpContext context, Forwarder forwarder) =>
-            {
-                HttpResponseMessage response = await forwarder.ForwardRequestAsync(context.Request); //För nuvarande skickar denna en ínkorrekt GET metod.
-
-                context.Response.StatusCode = (int)response.StatusCode;
-
-                foreach (var header in response.Headers)
+            {            
+                if (forwarder.CheckCache(context.Request, out HttpResponseMessage cachedBody))
                 {
-                    context.Response.Headers[header.Key] = header.Value.ToArray();
+                   
+                    await UpdateHttpContentAndHeaders(cachedBody, ref context);
+                    DisplayContextHeaders(context);
+
+                }
+                else
+                {
+                    HttpResponseMessage response = await forwarder.ForwardNewRequestAsync(context.Request); //This method forwards and also caches the conent body as string
+
+                    await UpdateHttpContentAndHeaders(response, ref context);
+
+                    DisplayContextHeaders(context);
                 }
 
-                foreach (var header in response.Content.Headers)
-                {
-                    context.Response.Headers[header.Key] = header.Value.ToArray();
-                }
-
-                context.Response.Headers.Remove("transfer-encoding");
-
-                var content = await response.Content.ReadAsStreamAsync();
-                await content.CopyToAsync(context.Response.Body);
 
 
             });
@@ -63,6 +63,37 @@ namespace CacheProxyServer
             await app.RunAsync();
 
         }
+
+        private static void DisplayContextHeaders(HttpContext context)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (var header in context.Response.Headers)
+            {
+                Console.WriteLine($"Headerkey: {header.Key}\n Headervalue: {header.Value}\n------");
+            }
+        }
+
+        private static async Task UpdateHttpContentAndHeaders(HttpResponseMessage response, ref HttpContext context)
+        {
+            context.Response.StatusCode = (int)response.StatusCode;
+
+            foreach (var header in response.Headers)
+            {
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            }
+
+            foreach (var header in response.Content.Headers)
+            {
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            }
+
+            context.Response.Headers.Remove("Transfer-Encoding");
+
+            await using Stream content = await response.Content.ReadAsStreamAsync();
+            await content.CopyToAsync(context.Response.Body);
+        }
+
+
         [Command("clear-cache", Description = "Clear the cache")]
         public void ClearCache()
         {
--- Forwarder.cs
+++ Forwarder.cs
@@ -1,30 +1,68 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using System.Net.Http.Headers;
 
 namespace CacheProxyServer
 {
     internal class Forwarder
     {
         private readonly HttpClient _httpClient;
+        private readonly IMemoryCache _cache;
 
 
-        public Forwarder(HttpClient httpClient)
+        public Forwarder(HttpClient httpClient, IMemoryCache cache)
         {
             _httpClient = httpClient;
+            _cache = cache;
         }
 
-        public async Task<HttpResponseMessage> ForwardRequestAsync(HttpRequest request)
+        public async Task<HttpResponseMessage> ForwardNewRequestAsync(HttpRequest request)
         {
             var path = new Uri(_httpClient.BaseAddress, request.Path + request.QueryString);
 
             var forwardRequest = new HttpRequestMessage(HttpMethod.Get, path);
 
+            HttpResponseMessage response = await _httpClient.SendAsync(forwardRequest);
 
-            var response = await _httpClient.SendAsync(forwardRequest);
+           
+            string key = request.Path + request.QueryString;
+            _cache.Set(key, response, TimeSpan.FromMinutes(5));
+            Console.WriteLine($"X-Cache: MISS");
 
-            //Create cache method here
-            return response;
+            string body = await response.Content.ReadAsStringAsync();
+
+            HttpResponseMessage newResponse = new HttpResponseMessage(response.StatusCode)
+            {
+                Content = new StringContent(body, System.Text.Encoding.UTF8, "application/json"), 
+            };
+
+            foreach (var headers in response.Headers)
+            {
+                newResponse.Headers.TryAddWithoutValidation(headers.Key, headers.Value);
+            }
+
+            foreach (var headers in response.Content.Headers)
+            {
+                newResponse.Content.Headers.TryAddWithoutValidation(headers.Key, headers.Value);
+            }
+
+            return newResponse;
+        }
 
 
+        public bool CheckCache(HttpRequest request, out HttpResponseMessage cachedResponse)
+        {
+            var path = new Uri(_httpClient.BaseAddress, request.Path + request.QueryString);
+            string key = request.Path + request.QueryString;
+            if (_cache.TryGetValue(key, out cachedResponse))
+            {
+                Console.WriteLine($"X-Cache: HIT");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
